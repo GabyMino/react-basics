7f8013b20d9ff8941ce80772205beaca51fa3124
MGM0821 <mgm0821@STL42MGM030822.local>
2020-06-10 15:03:00 -0500

    using props for table data

--- src/Table.js
+++ src/Table.js
@@ -1,27 +1,33 @@
 import React, {Component} from 'react'
 
+const TableHeader = () => {
+    return (
+        <thead>
+            <th>Name</th>
+            <th>Job</th>
+        </thead>
+    )
+}
+
+const TableBody = props => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        )
+    })
+    return <tbody>{rows}</tbody>
+}
+
 class Table extends Component {
     render(){
+        const { characterData } = this.props
         return (
             <table>
-                <thead>
-                    <th>Name</th>
-                    <th>Job</th>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Charlie</td>
-                        <td>Janitor</td>
-                    </tr>
-                    <tr>
-                        <td>Mac</td>
-                        <td>Bouncer</td>
-                    </tr>
-                    <tr>
-                        <td>Dennis</td>
-                        <td>Bartender</td>
-                    </tr>
-                </tbody>
+               <TableHeader />
+               <TableBody characterData={characterData}/>
             </table>
         )
     }
